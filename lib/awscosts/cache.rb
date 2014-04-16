3b3bfc0f04446bd01f6c43b5bd07c66e6d18e592
Steve Frank <lardcanoe@gmail.com>
2014-04-16 12:07:22 -0400

    Handle empty body with some retries

--- lib/awscosts/cache.rb
+++ lib/awscosts/cache.rb
@@ -14,8 +14,13 @@ module AWSCosts::Cache
   end
 
   def get_jsonp uri, base_uri = BASE_JSONP_URI, &block
+    attempts = 0
     cache[uri] ||= begin
       yield extract_json_from_callback(HTTParty.get("#{base_uri}#{uri}").body)
+    rescue NoMethodError
+      attempts += 1
+      retry if attempts < 5
+      raise "Failed to retrieve or parse data for #{base_uri}#{uri}"
     end
   end
 
