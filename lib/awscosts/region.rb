15fb01cc6f105f570176ad965c933190f118f818
awang <autumn.wang@illumio.com>
2017-03-02 10:55:38 -0800

    Add new aws regions, so that they are suported

--- lib/awscosts/region.rb
+++ lib/awscosts/region.rb
@@ -5,14 +5,21 @@ class AWSCosts::Region
 
   SUPPORTED =  {
     'us-east-1' => { :full_name => 'US (Northern Virginia)' },
+    'us-east-2' => { :full_name => 'US East (Ohio) Region' },
     'us-west-1' => { :full_name => 'US (Northern California)' },
     'us-west-2' => { :full_name => 'US (Oregon)' },
+    'ap-south-1' => {:full_name => 'Asia Pacific (Mumbai) Region' },
     'eu-west-1' => { :full_name => 'EU (Ireland)' },
     'eu-central-1' => { :full_name => 'EU (Frankfurt)' },
     'ap-southeast-1' => { :full_name => 'Asia Pacific (Singapore)' },
     'ap-southeast-2' => { :full_name => 'Asia Pacific (Sydney)' },
     'ap-northeast-1' => { :full_name => 'Asia Pacific (Tokyo)' },
-    'sa-east-1' => { :full_name => 'South America (Sao Paulo)' }
+    'ap-northeast-2' => { :full_name => 'Asia Pacific (Seoul) Region' },
+    'sa-east-1' => { :full_name => 'South America (Sao Paulo)' },
+    'us-gov-west-1' => { :full_name => 'AWS GovCloud (US)' },
+    'eu-west-2' => { :full_name => 'EU (London) Region' },
+    'cn-north-1' => { :full_name => 'China (Beijing) Region' },
+    'ca-central-1' => { :full_name => 'Canada (Central) Region' }
   }
 
   def self.find name
